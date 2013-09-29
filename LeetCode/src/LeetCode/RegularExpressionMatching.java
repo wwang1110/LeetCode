28d161ef38178baac3d707c00b251c49f17a7f24
weiywang <wwang1110@gmail.com>
2013-09-28 23:59:07 -0700

    n

---
+++ LeetCode/src/LeetCode/RegularExpressionMatching.java
@@ -0,0 +1,48 @@
+package LeetCode;
+
+public class RegularExpressionMatching {
+	//Implement regular expression matching with support for '.' and '*'.
+
+	//'.' Matches any single character.
+	//'*' Matches zero or more of the preceding element.
+
+	//The matching should cover the entire input string (not partial).
+
+	//The function prototype should be:
+	//bool isMatch(const char *s, const char *p)
+
+	//Some examples:
+	//isMatch("aa","a")  false
+	//isMatch("aa","aa")  true
+	//isMatch("aaa","aa")  false
+	//isMatch("aa", "a*")  true
+	//isMatch("aa", ".*")  true
+	//isMatch("ab", ".*")  true
+	//isMatch("aab", "c*a*b")  true
+    public boolean isMatch(String s, String p) {
+    	return isMatch(s,0,p,0);
+    }
+
+    private boolean isMatch(String s, int i, String p, int j) {
+    	if (j < p.length())
+    	{
+    		if ((j+1<p.length()&&p.charAt(j+1)!='*')||j+1==p.length())
+    		{
+    			if ((p.charAt(j)=='.')||p.charAt(j) == s.charAt(i))
+    				return isMatch(s, i+1, p, j+1);
+    		}
+    		else if (j+1<p.length()&&p.charAt(j+1)=='*')
+    		{
+    			while(i < s.length() && s.charAt(i)==p.charAt(j)||p.charAt(j)=='.')
+    			{
+    				if(isMatch(s, i, p, j+2))return true;
+    				i++;
+    			}
+    			return isMatch(s, i, p, j+2);
+    		}
+    	}
+    	else if (i==s.length())
+    		return true;
+   		return false;
+    }
+}
---
+++ LeetCode/src/LeetCode/RegularExpressionMatchingTest.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RegularExpressionMatchingTest {
+
+	@Test
+	public void test() {
+		//isMatch("aa","a")  false
+		//isMatch("aa","aa")  true
+		//isMatch("aaa","aa")  false
+		//isMatch("aa", "a*")  true
+		//isMatch("aa", ".*")  true
+		//isMatch("ab", ".*")  true
+		//isMatch("aab", "c*a*b")  true
+		RegularExpressionMatching solution = new RegularExpressionMatching();
+		Assert.assertEquals(false, solution.isMatch("aa", "a"));
+		Assert.assertEquals(true, solution.isMatch("aa", "aa"));
+		Assert.assertEquals(false, solution.isMatch("aaa", "aa"));
+		Assert.assertEquals(true, solution.isMatch("aa", "a*"));
+		Assert.assertEquals(false, solution.isMatch("aa", "a"));
+		Assert.assertEquals(false, solution.isMatch("aa", "a"));
+		Assert.assertEquals(false, solution.isMatch("aa", "a"));
+	}
+
+}
