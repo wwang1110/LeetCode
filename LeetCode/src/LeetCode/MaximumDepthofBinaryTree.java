359d7ae0abdc9b3f487d13ca48444d3499ce5db3
wwang1110 <wwang1110@gmail.com>
2013-10-15 16:26:02 -0700

    n

---
+++ LeetCode/src/LeetCode/MaximumDepthofBinaryTree.java
@@ -0,0 +1,12 @@
+package LeetCode;
+
+public class MaximumDepthofBinaryTree {
+	//Given a binary tree, find its maximum depth.
+
+	//The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+    public int maxDepth(TreeNode root) {
+    	if (root == null)
+    		return 0;
+    	return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }	
+}
