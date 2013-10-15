9236cd062436cf6dcafec0f69c3c81ccc740352f
wwang1110 <wwang1110@gmail.com>
2013-10-15 13:20:34 -0700

    n

---
+++ LeetCode/src/LeetCode/LargestRectangleinHistogram.java
@@ -0,0 +1,13 @@
+package LeetCode;
+
+public class LargestRectangleinHistogram {
+	//Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, 
+	//find the area of largest rectangle in the histogram.
+	//For example,
+	//Given height = [2,1,5,6,2,3],
+	//return 10.
+    public int largestRectangleArea(int[] height) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        return 0;
+    }	
+}
