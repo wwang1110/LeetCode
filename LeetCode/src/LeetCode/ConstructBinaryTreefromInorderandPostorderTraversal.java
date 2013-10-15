e97800da4fa89709f59e63f3c202105c8bbbfd7b
wwang1110 <wwang1110@gmail.com>
2013-10-15 16:31:58 -0700

    n

---
+++ LeetCode/src/LeetCode/ConstructBinaryTreefromInorderandPostorderTraversal.java
@@ -0,0 +1,12 @@
+package LeetCode;
+
+public class ConstructBinaryTreefromInorderandPostorderTraversal {
+	//Given inorder and postorder traversal of a tree, construct the binary tree.
+
+	//Note:
+	//You may assume that duplicates do not exist in the tree.
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+    	return null;
+    }	
+}
