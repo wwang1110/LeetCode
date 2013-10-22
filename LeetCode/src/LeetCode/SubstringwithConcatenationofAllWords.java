6b7329d3b8b81a666525048c5320c2f57714f80f
weiywang <wwang1110@gmail.com>
2013-10-22 00:33:45 -0700

    m

--- LeetCode/src/LeetCode/SubstringwithConcatenationofAllWords.java
+++ LeetCode/src/LeetCode/SubstringwithConcatenationofAllWords.java
@@ -1,7 +1,6 @@
 package LeetCode;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class SubstringwithConcatenationofAllWords {
 	//You are given a string, S, and a list of words, L, that are all of the same length. 
@@ -15,40 +14,62 @@ public class SubstringwithConcatenationofAllWords {
 	//You should return the indices: [0,9].
 	//(order does not matter).
     public ArrayList<Integer> findSubstring(String S, String[] L) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
     	ArrayList<Integer> ret = new ArrayList<Integer>();
-    	int wordlen = L[0].length();
-    	int fulllen = wordlen * L.length;
-    	
-    	List<String> words = new ArrayList<String>();
-    	for (String w : L)
-    		words.add(w);
-    	
-        for (int i = 0; i < S.length() - fulllen; i++)
-        {
-        	if (matchSubstring(S, i, words, wordlen) == true)
-        		ret.add(i);
-        }
-        return ret;
-    }
-    private boolean matchSubstring(String S, int idx, List<String> words, int wordlen) {
-    	if (words.size() == 0)
-    		return true;
-    	if (idx + wordlen < S.length())
+    	int len = S.length();
+    	int wlen = L[0].length();
+    	ArrayList<Integer> ns = new ArrayList<Integer>();
+    	int i = 0;
+    	while (i < len)
     	{
-    		String word = S.substring(idx, idx + wordlen);
-    		for (String w : words)
+    		boolean found = false;
+    		if (i + wlen < len)
     		{
-    			if (w.equals(word))
-    			{
-    				words.remove(w);
-    				boolean find = matchSubstring(S, idx + wordlen, words, wordlen);
-    				words.add(w);
-    				return find;
-    			}
+	    		for (int j = 0; j < L.length; j++)
+	    			if (S.substring(i, i + wlen).equalsIgnoreCase(L[j]))
+	    			{
+	    				ns.add(j);
+	    				found = true;
+	    				break;
+	    			}
+    		}
+    		if (found == true)
+    			i += wlen;
+    		else
+    		{
+    			ns.add(-1);
+    			i ++;
     		}
     	}
-    	return false;
+    	
+    	
+    	int marks[] = new int [L.length];
+    	int start = 0;
+    	int minusOne = 0;
+    	for (int k = 0; k < ns.size(); k++)
+    	{
+    		if (ns.get(k) != -1)
+    		{
+    			marks[ns.get(k)]++;
+    			while (ns.get(k) > 1)
+    				marks[ns.get(start++)]--;
+    			
+    			boolean completed = true;
+    			for (int m : marks)
+    				if (m == 0)
+    					completed = false;
+    			
+    			if (completed == true)
+    				ret.add((start - minusOne)*wlen + minusOne);
+    		}
+    		else
+    		{
+    			minusOne++;
+    			for (int m = 0; m < marks.length; m++)
+    				marks[m] = 0;
+    			start = k + 1;
+    		}
+    	}
+    	
+        return ret;
     }
 }
--- LeetCode/src/LeetCode/SubstringwithConcatenationofAllWordsTest.java
+++ LeetCode/src/LeetCode/SubstringwithConcatenationofAllWordsTest.java
@@ -7,11 +7,18 @@ import org.junit.Test;
 public class SubstringwithConcatenationofAllWordsTest {
 
 	@Test
-	public void test() {
+	public void test1() {
 		SubstringwithConcatenationofAllWords s = new SubstringwithConcatenationofAllWords();
 		ArrayList<Integer> ret = s.findSubstring("barfoothefoobarman", new String[] {"foo", "bar"});
 		Assert.assertEquals(0, (int)ret.get(0));
 		Assert.assertEquals(9, (int)ret.get(1));
 	}
 
+	@Test
+	public void test2() {
+		SubstringwithConcatenationofAllWords s = new SubstringwithConcatenationofAllWords();
+		ArrayList<Integer> ret = s.findSubstring("lingmindraboofooowingdingbarrwingmonkeypoundcake", new String[] {"fooo", "barr", "wing","ding","wing"});
+		Assert.assertEquals(0, (int)ret.get(0));
+		Assert.assertEquals(9, (int)ret.get(1));
+	}
 }
