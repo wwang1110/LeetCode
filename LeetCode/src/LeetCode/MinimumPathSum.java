64d7548f777e555168271128b977a6d6cb6ab87a
wwang1110 <wwang1110@gmail.com>
2013-10-24 10:38:54 -0700

    n

--- LeetCode/src/LeetCode/MinimumPathSum.java
+++ LeetCode/src/LeetCode/MinimumPathSum.java
@@ -11,12 +11,13 @@ public class MinimumPathSum {
     	int m = grid.length;
     	int n = grid[0].length;
         int step[][] = new int[m][n];
+        step[0][0] = grid[0][0];
         
-        for (int i = 0; i < m; i++)
-        	step[i][0] = grid[i][0];
+        for (int i = 1; i < m; i++)
+        	step[i][0] = step[i - 1][0] + grid[i][0];
 
-        for (int i = 0; i < n; i++)
-        	step[0][i] = grid[0][i];
+        for (int i = 1; i < n; i++)
+        	step[0][i] = step[0][i - 1] + grid[0][i];
         
         for (int i = 1; i < m; i++)
         	for (int j = 1; j < n; j++)
--- LeetCode/src/LeetCode/MinimumPathSumTest.java
+++ LeetCode/src/LeetCode/MinimumPathSumTest.java
@@ -6,9 +6,14 @@ import org.junit.Test;
 public class MinimumPathSumTest {
 
 	@Test
-	public void test() {
+	public void test1() {
 		MinimumPathSum s = new MinimumPathSum();
 		Assert.assertEquals(3, s.minPathSum(new int[][]{{1,1},{1,2}}));
+	}
+	@Test
+	public void test2() {
+		MinimumPathSum s = new MinimumPathSum();
+		Assert.assertEquals(7, s.minPathSum(new int[][]{{1,3,1},{1,5,1},{4,2,1}}));
 	}
 
 }
