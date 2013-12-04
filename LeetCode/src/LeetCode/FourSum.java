d99ef7c04b01eecabe9c97ea4c44da2643ad188b
wwang1110 <wwang1110@gmail.com>
2013-12-04 14:28:21 -0800

    4sum

--- LeetCode/src/LeetCode/FourSum.java
+++ LeetCode/src/LeetCode/FourSum.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class FourSum {
 	//Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
@@ -18,41 +19,38 @@ public class FourSum {
 	//(-2, -1, 1, 2)
 	//(-2,  0, 0, 2)
     public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
-    	HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+
     	Arrays.sort(num);
-    	
-    	ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
-    	for (int i = 0; i < num.length; i++)
-    		for (int j = i + 1; j < num.length; j++)
-    		{
-    			if (map.containsKey(-num[i]-num[j]))
-    			{
-    				ArrayList<Integer> t = map.get(-num[i]-num[j]);
-    				if (t.get(0) != i && t.get(0) != j && t.get(1) != i && t.get(1) != j)
-    				{
-        				ArrayList<Integer> quadruplet = new ArrayList<Integer>();
-        				quadruplet.add(num[t.get(0)]);
-        				quadruplet.add(num[t.get(1)]);
-        				quadruplet.add(num[i]);
-        				quadruplet.add(num[j]);
-        				Collections.sort(quadruplet);
-        				
-        				boolean dup = false;
-        				for (ArrayList<Integer> q : ret)
-        				{
-        					if (q.get(0) == quadruplet.get(0) && q.get(1) == quadruplet.get(1)
-        						&&q.get(2) == quadruplet.get(2) && q.get(3) == quadruplet.get(3))
-        						dup = true;
-        				}
-        				if (dup == false)
-        					ret.add(quadruplet);
-    				}
-    			}
-    			ArrayList<Integer> item = new ArrayList<Integer>();
-    			item.add(i);
-    			item.add(j);
-    			map.put(num[i]+num[j], item);
-    		}
-    	return ret;
+        HashSet<ArrayList<Integer>> hSet = new HashSet<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i + 1; j < num.length; j++) {
+                for (int k = j + 1, l = num.length - 1; k < l;) {
+                    int sum = num[i] + num[j] + num[k] + num[l];
+                    if (sum > target) {
+                        l--;
+                    }
+                    else if (sum < target) {
+                        k++;
+                    }
+                    else if (sum == target) {
+                        ArrayList<Integer> found = new ArrayList<Integer>();
+                        found.add(num[i]);
+                        found.add(num[j]);
+                        found.add(num[k]);
+                        found.add(num[l]);
+                        if (!hSet.contains(found)) {
+                            hSet.add(found);
+                            result.add(found);
+                        }
+
+                        k++;
+                        l--;
+
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
