49fb7d665b972fd52f6459881a888c99e13c0e6c
wwang1110 <wwang1110@gmail.com>
2013-12-18 17:37:16 -0800

    n

--- LeetCode/src/LeetCode/UniqueBinarySearchTreesIITest.java
+++ LeetCode/src/LeetCode/UniqueBinarySearchTreesIITest.java
@@ -1,9 +1,8 @@
 package LeetCode;
 
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UniqueBinarySearchTreesIITest {
@@ -12,8 +11,11 @@ public class UniqueBinarySearchTreesIITest {
 	public void test() {
 		UniqueBinarySearchTreesII s = new UniqueBinarySearchTreesII();
 		ArrayList<TreeNode> r1 = s.generateTrees(1);
+		Assert.assertEquals(1, r1.size());
 		ArrayList<TreeNode> r2 = s.generateTrees(2);
+		Assert.assertEquals(2, r2.size());
 		ArrayList<TreeNode> r3 = s.generateTrees(3);
+		Assert.assertEquals(5, r3.size());
 	}
 
 }
