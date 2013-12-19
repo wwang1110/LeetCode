0d3fa0621912c34c76904299a20f6beccabcff20
wwang1110 <wwang1110@gmail.com>
2013-12-19 11:44:21 -0800

    n

--- LeetCode/src/LeetCode/DistinctSubsequencesTest.java
+++ LeetCode/src/LeetCode/DistinctSubsequencesTest.java
@@ -8,6 +8,7 @@ public class DistinctSubsequencesTest {
 	@Test
 	public void test() {
 		DistinctSubsequences s = new DistinctSubsequences();
+		Assert.assertEquals(1, s.numDistinct("ABCDE", "ACE"));
 		Assert.assertEquals(3, s.numDistinct("rabbbit", "rabbit"));
 	}
 
