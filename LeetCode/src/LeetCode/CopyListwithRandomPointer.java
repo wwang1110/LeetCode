b6f510182b16999a6559d895d59ff6fae0d98120
wwang1110 <wwang1110@gmail.com>
2014-02-12 15:24:56 -0800

    m

--- LeetCode/src/LeetCode/CopyListwithRandomPointer.java
+++ LeetCode/src/LeetCode/CopyListwithRandomPointer.java
@@ -1,51 +1,31 @@
 package LeetCode;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CopyListwithRandomPointer {
 	//A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
 
 	//Return a deep copy of the list.
-	   public RandomListNode copyRandomList(RandomListNode head) {
-			if (head == null)
-				return null;
-			
-			HashMap<RandomListNode, Integer> map = new HashMap<RandomListNode, Integer>(); 
-			ArrayList<RandomListNode> nodes = new ArrayList<RandomListNode>(); 
-			int idx = 0;
-			RandomListNode node = head;
-			while (node != null)
-			{
-				map.put(node, idx++);
-				nodes.add(new RandomListNode(node.label));
-				node = node.next;
-			}
-			
-			ArrayList<Integer> links = new ArrayList<Integer>();
-			node = head;
-			while (node != null)
-			{
-				if (node.random == null)
-					links.add(-1);
-				else
-					links.add(map.get(node.random));
-				node = node.next;
-			}
-			
-			for (int i = 0; i < nodes.size(); i++)
-			{
-				if (i != nodes.size() -1)
-					nodes.get(i).next = nodes.get(i+1);
-				else
-					nodes.get(i).next = null;
-				
-				if (links.get(i) == -1)
-					nodes.get(i).random = null;
-				else
-					nodes.get(i).random = nodes.get(links.get(i));
-			}
-			
-			return nodes.get(0);
-	    }
+	HashMap<RandomListNode, RandomListNode> map;
+	public RandomListNode copyRandomList(RandomListNode head) {
+		if (head == null)
+			return null;
+		
+		map = new HashMap<RandomListNode, RandomListNode>();
+		map.put(null, null);
+		return copy(head);
+    }
+
+	private RandomListNode copy(RandomListNode node) {
+		if (!map.containsKey(node))
+		{
+			RandomListNode newNode = new RandomListNode(node.label);
+			map.put(node, newNode);
+			newNode.next = copy(node.next);
+			newNode.random = copy(node.random);
+			return newNode;
+		}
+		
+		return map.get(node);
+	}
 }
